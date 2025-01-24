b04368304f7c80cd49f08ad93da2fb1c5ee9caf1
Maxfield Ma <mma@haverford.edu>
2025-01-24 15:48:40 -0500

    fixed spaces for tabs

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -8,33 +8,33 @@
 #include <stdlib.h>
 
 int main() {
-	printf("Welcome to Math Game!\n");
-	printf("How many rounds do you want to play? ");
-	int amountOfRounds = 0;
-	scanf("%d", &amountOfRounds);
+    printf("Welcome to Math Game!\n");
+    printf("How many rounds do you want to play? ");
+    int amountOfRounds = 0;
+    scanf("%d", &amountOfRounds);
 
-	//initializing all the variables used
-	short number1;
-	short number2;
-	short answer;
-	short correctAnswers = 0;
+    //initializing all the variables used
+    short number1;
+    short number2;
+    short answer;
+    short correctAnswers = 0;
 
-	for(int i = 0; i < amountOfRounds; i++){
-		//setting the two numbers to a random value between 1 - 10
-		number1 = rand() % 9 + 1;
-		number2 = rand() % 9 + 1;
+    for(int i = 0; i < amountOfRounds; i++){
+        //setting the two numbers to a random value between 1 - 10
+        number1 = rand() % 9 + 1;
+        number2 = rand() % 9 + 1;
 
-		printf("\n%hi + %hi = ? ", number1, number2);
-		scanf("%hi", &answer);
+        printf("\n%hi + %hi = ? ", number1, number2);
+        scanf("%hi", &answer);
 
-		if (answer == number1 + number2){
-			printf("Correct!\n");
-			correctAnswers += 1;
-		} else {
-			printf("Incorrect :(\n");
-		}
-	}
+        if (answer == number1 + number2){
+            printf("Correct!\n");
+            correctAnswers += 1;
+        } else {
+            printf("Incorrect :(\n");
+        }
+    }
 
-	printf("You answered %hi/%hi correctly.\n", correctAnswers, amountOfRounds);
-	return 0;
+    printf("You answered %hi/%hi correctly.\n", correctAnswers, amountOfRounds);
+    return 0;
 }
