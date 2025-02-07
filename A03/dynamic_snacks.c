9ec43c6469a32fca7324ca54f7c192a0bdd08fe1
Maxfield Ma <mma@haverford.edu>
2025-02-07 15:52:15 -0500

    2nd core done

--- A03/dynamic_snacks.c
+++ A03/dynamic_snacks.c
@@ -6,7 +6,43 @@
 
  //Testing
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct snack{
+    char name[50];
+    float cost;
+    int quantity;
+};
+
+//quick function to print out the snack menu with some nice formatting :D
+void printInfo( struct snack bar[], int size) {
+    printf("\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d) %-15s\t cost: $%-10.2f quantity: %-10d\n", 
+                i, bar[i].name, bar[i].cost, bar[i].quantity);
+    }
+}
+
 
 int main() {
+    int amount;
+    
+    printf("Enter number of snacks: ");
+    scanf("%d", &amount);
+
+    struct snack* snackBar = malloc(sizeof(struct snack) * amount);
+    for(int i = 0; i < amount; i++){
+        printf("Enter a name: ");
+        char name[50];
+        scanf("%s", snackBar[i].name);
+        printf("Enter a cost: $");
+        scanf("%f", &snackBar[i].cost);
+        printf("Enter a quantity: ");
+        scanf("%d", &snackBar[i].quantity);
+    }
+    printf("\nWelcome to Dynamic Donna's Snack Bar\n");
+    printInfo( snackBar, amount);
+    free(snackBar);
   return 0;
 }
--- A03/repeat.c
+++ A03/repeat.c
@@ -1,10 +1,45 @@
 /*----------------------------------------------
- * Author: 
+ * Author: Maxfield Ma
  * Date: 
  * Description
  ---------------------------------------------*/
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+char* repeat(char* input, int amount){
+    char* newWord = malloc(strlen(input)*amount+1);
+
+    if(!newWord){
+        return NULL;
+    }
+
+    for( int i = 0; i < strlen(input)*amount; i++){
+        newWord[i] = input[i % strlen(input)];
+    }
+    newWord[strlen(newWord)] = '\0';
+    return newWord;
+}
 
 int main() {
+    char word[32];
+    int count;
+    printf("Enter a word: ");
+    scanf("%s", word);
+    printf("Enter a count: ");
+    scanf("%d", &count);
+    char* newWord = repeat(word, count);
+    if(newWord){
+
+    
+        printf("Your word is %s\n", newWord);
+        free(newWord);
+    } else {
+        
+        printf("Cannot allocate new string. Exiting...\n");
+    }
+
+
   return 0;
 }
