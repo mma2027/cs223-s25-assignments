39c005cddbf660250d0170528b8e04de6219c655
Maxfield Ma <mma@haverford.edu>
2025-03-25 16:04:35 -0400

    finished

--- J04/message_reader.c
+++ J04/message_reader.c
@@ -1,4 +1,47 @@
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct meta_data
 {
+  float version;
+  unsigned int year;
+  unsigned int length;
+};
+
+void read(const char* filename) {
+  FILE* fp = fopen(filename, "rb");
+  if(fp == NULL){
+      printf("Filename invalid\n");
+  }
+  char comment[40];
+  fgets(comment, 40, fp);
+  printf("header: %s", comment);
+
+  struct meta_data* data = malloc(sizeof(struct meta_data));
+
+  fread(data, sizeof(struct meta_data), 1, fp);
+
+
+  printf("Version: %.2f\n", data->version);
+  printf("Year: %d\n", data->year);
+  printf("Length: %d\n", data->length);
+
+  char* text = malloc((data->length + 1));
+  fread(text, (data->length + 1), 1, fp);
+  printf("Message: %s\n", text);
+  fclose(fp);
+  free(data);
+  free(text);
+}
+
+
+int main(int argc, char* argv[])
+{
+  if ( argc != 2){
+    printf("Usage: ./message_reader\n");
+    return 0;
+  }
+  read(argv[1]);
   return 0;
 }
--- J04/message_writer.c
+++ J04/message_writer.c
@@ -1,4 +1,47 @@
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct meta_data
 {
+  float version;
+  unsigned int year;
+  unsigned int length;
+};
+
+
+void write(const char* filename) {
+  // Open the file in binary write mode and return NULL if it can't be opened
+  FILE *fp = fopen(filename, "wb");
+  if (fp == NULL) {
+      printf("Error opening file %s.\n", filename);
+      return;
+  }
+
+  char *comment = "# comment\n";
+
+  fwrite(comment, sizeof(char), strlen(comment), fp);
+
+  struct meta_data* temp = malloc(sizeof(struct meta_data));
+  temp->version = 0.1;
+  temp->year = 1990;
+  temp->length = 6;
+  
+  char* message = "hi :)";
+
+  fwrite(temp, sizeof(struct meta_data), 1, fp);
+  fwrite(message, sizeof(char), strlen(message), fp);
+
+  fclose(fp);  // Close the file
+  free(temp);
+}
+
+int main(int argc, char* argv[])
+{
+  if ( argc != 2){
+    printf("Usage: ./message_wrter\n");
+    return 0;
+  }
+
+  write(argv[1]);
   return 0;
 }
