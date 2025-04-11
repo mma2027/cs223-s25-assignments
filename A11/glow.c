9b1aa1bd6c6c54c3ffcf552afc07e53ccefbeb33
Maxfield Ma <mma@haverford.edu>
2025-04-11 11:58:58 -0400

    in progress

--- A11/glow.c
+++ A11/glow.c
@@ -8,6 +8,119 @@
 #include "write_ppm.h"
 
 int main(int argc, char* argv[]) {
-  // todo: your code here
-  // compute image
+    // todo: your code here
+    // compute image
+    int w;
+    int h;
+    struct ppm_pixel** pixels = read_ppm_2d("earth-small.ppm", &w, &h);
+    
+    //failsafe
+    if (pixels == NULL) {
+        printf("Error: Failed to read the image\n");
+        return 1;  // Indicate error state
+    }
+
+    struct ppm_pixel** blur = malloc(h * sizeof(struct ppm_pixel*));
+    for (int i = 0; i < h; i++){
+        blur[i] = malloc(w * sizeof(struct ppm_pixel));
+        //checking if there is enough space
+        if (!blur[i]) {
+            //freeing the rest
+            for (int j = 0; j < i; j++) {
+                free(blur[j]);
+            }
+            free(blur);
+        }
+    }
+
+    struct ppm_pixel** bright = malloc(h * sizeof(struct ppm_pixel*));
+    for (int i = 0; i < h; i++){
+        bright[i] = malloc(w * sizeof(struct ppm_pixel));
+        //checking if there is enough space
+        if (!bright[i]) {
+            //freeing the rest
+            for (int j = 0; j < i; j++) {
+                free(bright[j]);
+            }
+            free(bright);
+        }
+    }
+
+    for (int y = 0; y < h; y++){
+        for (int x = 0; x < w; x++){
+            if (pixels[y][x].red + pixels[y][x].green + pixels[y][x].blue > 600){
+                bright[y][x].red = pixels[y][x].red;
+                bright[y][x].green = pixels[y][x].green;
+                bright[y][x].blue = pixels[y][x].blue;
+            } else {
+                bright[y][x].red = 0;
+                bright[y][x].green = 0;
+                bright[y][x].blue = 0;
+            }
+        }
+    }
+
+    for (int y = 0; y < h; y++){
+        for (int x = 0; x < w; x++){
+            int startX = x - 12;
+            int endX = x + 13;
+            int startY = y - 12;
+            int endY = y + 13;
+
+            if (startX < 0) startX = 0;
+            if (endX > w) endX = w;
+            if (startY < 0) startY = 0;
+            if (endY > h) endY = h;
+
+            int sumR = 0, sumG = 0, sumB = 0;
+            int count = 0;
+            for( int i = startY; i < endY; i++ ){
+                for ( int j = startX; j < endX; j++ ){
+                    sumR += bright[i][j].red;
+                    sumG += bright[i][j].green;
+                    sumB += bright[i][j].blue;
+                    count++;
+                }
+            }
+            blur[y][x].red = sumR / count;
+            blur[y][x].green = sumG / count;
+            blur[y][x].blue = sumB / count;
+        }
+    }
+
+    
+    for (int y = 0; y < h; y++){
+        for (int x = 0; x < w; x++){
+            if (pixels[y][x].red + blur[y][x].red > 255) {
+                pixels[y][x].red = 255;
+            } else {
+                pixels[y][x].red += blur[y][x].red;
+            }
+
+            if (pixels[y][x].blue + blur[y][x].blue > 255) {
+                pixels[y][x].blue = 255;
+            } else {
+                pixels[y][x].blue += blur[y][x].blue;
+            }
+
+            if (pixels[y][x].green + blur[y][x].green > 255) {
+                pixels[y][x].green = 255;
+            } else {
+                pixels[y][x].green += blur[y][x].green;
+            }
+
+        }
+    }
+    
+    write_ppm_2d("glow.ppm", pixels, w, h);
+
+    //free malloc
+    for (int i = 0; i < h; i++) {
+        free(pixels[i]);
+        free(blur[i]);
+        free(bright[i]);
+    }
+    free(pixels);
+    free(blur);
+    free(bright);
 }
--- A11/write_ppm.c
+++ A11/write_ppm.c
@@ -5,5 +5,29 @@
 void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
 }
 
-void write_ppm_2d(const char* filename, struct ppm_pixel** pxs, int w, int h) {
+void write_ppm_2d(const char* filename, struct ppm_pixel** pixels, int w, int h) {
+    // Open the file in binary write mode and return NULL if it can't be opened
+    FILE *fp = fopen(filename, "wb");
+    if (fp == NULL) {
+        printf("Error opening file %s.\n", filename);
+        return;
+    }
+
+    //header information
+    fprintf(fp, "P6\n");
+    fprintf(fp, "#\n"); 
+    fprintf(fp, "%d %d\n", w, h);  
+    fprintf(fp, "255\n");           
+
+    // Write the pixel data
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            // Write the RGB values for each pixel
+            fwrite(&pixels[i][j].red, sizeof(unsigned char), 1, fp);
+            fwrite(&pixels[i][j].green, sizeof(unsigned char), 1, fp);
+            fwrite(&pixels[i][j].blue, sizeof(unsigned char), 1, fp);
+        }
+    }
+
+    fclose(fp);  // Close the file
 }
